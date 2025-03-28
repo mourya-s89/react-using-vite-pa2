fe6ec910122a34dab4ff82a0c51a90bdfb248d01
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-28 13:37:33 +0530

    Completed

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>kadali Mourya</h1>
+      <p>kadali.mourya.s89.kalvium.community</p>
+    </div>
+  );
+};
+
+export default Home;
